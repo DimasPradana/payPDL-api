328f23ea7e40f67865591557ed6fe781601f9aab
DimasPradana <deemaazbaek@gmail.com>
2021-06-25 18:00:37 +0700

    tambah lunas

--- controller/pbb/inquiry.go
+++ controller/pbb/inquiry.go
@@ -25,7 +25,9 @@ var InqStatusError model.InqStatusError
 var nama model.InqNama
 var arrNama []model.InqNama
 var denda float64
-var lunas []byte
+var lunas, statusLunas, statusSPPT byte
+
+var arrLunas []byte
 var jatuhtempo string
 
 func Inquiry(c *gin.Context) {
@@ -57,6 +59,9 @@ func Inquiry(c *gin.Context) {
 	tahun := reqInquiry.MASAPAJAK
 	nop := kdKecamatan + "-" + kdKelurahan + "-" + kdBlok + "-" + noUrut + "-" + kdJnsOp
 
+	//statusLns := cekLunas(kdKecamatan, kdKelurahan, kdBlok, noUrut, kdJnsOp, tahun)
+	//logrus.Infof("baris 63 isi dari statusLns: %v", statusLns)
+
 	/*
 		TODO snub on Sab 04 Jul 2020 11:11:16  : query ke database masuk variabel
 	*/
@@ -73,7 +78,7 @@ func Inquiry(c *gin.Context) {
 		"and a.NO_URUT = '%v' "+
 		"and a.KD_JNS_OP = '%v' "+
 		"and a.thn_pajak_sppt <= '%v' "+
-		"and a.STATUS_PEMBAYARAN_SPPT = 0 "+
+		"and a.STATUS_PEMBAYARAN_SPPT = '0' "+
 		"group by a.THN_PAJAK_SPPT, a.PBB_YG_HARUS_DIBAYAR_SPPT, a.TGL_JATUH_TEMPO_SPPT, a.NM_WP_SPPT, "+
 		"a.STATUS_PEMBAYARAN_SPPT, b.NM_KELURAHAN "+
 		"order by a.THN_PAJAK_SPPT desc) where ROWNUM <= 11", kdKecamatan, kdKelurahan, kdBlok, noUrut, kdJnsOp, tahun)
@@ -95,59 +100,73 @@ func Inquiry(c *gin.Context) {
 	// logrus.Infof("kon : %v", kon)
 	logrus.Infof("row 95 inquiry request nop : %v", nop)
 
-	rows, err := kon.Query(qryFix)
-	if err != nil {
-		logrus.Infof("errornya di baris 99 : %v,", qryFix)
-		http.Error(c.Writer, err.Error(), 500)
-		return
-	}
+	statusSPPT = cekSPPT(kdKecamatan, kdKelurahan, kdBlok, noUrut, kdJnsOp, tahun)
 
-	for rows.Next() {
-		//if err != rows.Scan(&tagihan.TAHUN, &tagihan.POKOK, &tagihan.JATUHTEMPO, &nama.NAMA, &tagihan.LUNAS, &nama.KELURAHAN) {
-		if err != rows.Scan(&tagihan.TAHUN, &tagihan.POKOK, &jatuhtempo, &nama.NAMA, &lunas, &nama.KELURAHAN) {
+	if statusSPPT != 0 {
+		rows, err := kon.Query(qryFix)
+		if err != nil {
+			logrus.Infof("errornya di baris 99 : %v,", qryFix)
 			http.Error(c.Writer, err.Error(), 500)
 			return
 		}
 
-		t := jatuhtempo[0:10]
-		_t, err := time.Parse("2006-01-02", t)
-		if err != nil {
-			logrus.Error(err)
-			return
+		for rows.Next() {
+			//if err != rows.Scan(&tagihan.TAHUN, &tagihan.POKOK, &tagihan.JATUHTEMPO, &nama.NAMA, &tagihan.LUNAS, &nama.KELURAHAN) {
+			if err != rows.Scan(&tagihan.TAHUN, &tagihan.POKOK, &jatuhtempo, &nama.NAMA, &lunas, &nama.KELURAHAN) {
+				http.Error(c.Writer, err.Error(), 500)
+				return
+			}
+
+			t := jatuhtempo[0:10]
+			_t, err := time.Parse("2006-01-02", t)
+			if err != nil {
+				logrus.Error(err)
+				return
+			}
+
+			statusLunas = cekLunas(kdKecamatan, kdKelurahan, kdBlok, noUrut, kdJnsOp, tahun)
+
+			tagihan.DENDA = uint64(ambilDenda(_t, tagihan.POKOK))
+			tagihan.TOTAL = tagihan.POKOK + tagihan.DENDA
+
+			arrTagihan = append(arrTagihan, tagihan)
+			arrNama = append(arrNama, nama)
+			arrLunas = append(arrLunas, lunas)
+			//logrus.Infof("isi dari scan\nNama : %v,\nTahun: %v\nTagihan: %v\nlunas :%v", arrNama[0].NAMA, tagihan.TAHUN, tagihan.POKOK, lunas[0])
+			//logrus.Infof("\narray nama : %v, tahun : %v,lunas : %v", nama.NAMA, tagihan.TAHUN, lunas)
 		}
-
-		tagihan.DENDA = uint64(ambilDenda(_t, tagihan.POKOK))
-		tagihan.TOTAL = tagihan.POKOK + tagihan.DENDA
-
-		arrTagihan = append(arrTagihan, tagihan)
-		arrNama = append(arrNama, nama)
-		// logrus.Infof("isi dari scan\nNama : %v,\nTahun: %v\nTagihan: %v", arrNama[0].NAMA, tagihan.TAHUN, tagihan.POKOK)
-		// logrus.Infof("\narray nama : %v", arrNama)
+		defer rows.Close()
 	}
-	defer rows.Close()
-	//logrus.Infof("isi dari scan\nNama : %v,\nPokok: %v\nDenda: %v\nTotal: %v", arrNama[0].NAMA, arrTagihan[2].POKOK, arrTagihan[2].DENDA, arrTagihan[2].TOTAL)
 
+	//logrus.Infof("status lunas tahun berjalan : %v, DOP : %v", statusLunas, statusSPPT)
+
+	/*logrus.Infof("isi dari scan\nNama : %v,\nPokok: %v\nDenda: %v\nTotal: %v\ntahun : %v\nlunas : %v", arrNama[0].NAMA,
+	arrTagihan[0].POKOK, arrTagihan[0].DENDA, arrTagihan[0].TOTAL, arrTagihan[0].TAHUN, arrLunas[0])
+	*/
 	/*
 		TODO snub on Sab 04 Jul 2020 11:13:59  : inisialisasi respon body
 	*/
 	switch {
 	//case arrNama[0].NAMA == "":
-	case arrNama == nil:
+	//case arrNama == nil:
+	case statusSPPT == 0:
 		{
 			InqError.ISERROR = "True"
 			InqError.RESPONSECODE = "10"
 			InqError.ERRORDESC = "Data SPPT dengan Tahun pajak tersebut tidak terdapat pada database"
 			InqStatusError.STATUSERROR = InqError
-			arrTagihan, arrNama = nil, nil
+			arrTagihan, arrNama, arrLunas = nil, nil, nil
 			c.JSON(http.StatusOK, InqStatusError)
 		}
-	case lunas[0] == 1:
+	//case lunas[0] == 49:
+	//case arrLunas[0] == 1:
+	case statusLunas == 1&statusSPPT == true:
 		{
 			InqError.ISERROR = "True"
 			InqError.RESPONSECODE = "13"
 			InqError.ERRORDESC = "Tagihan SPPT dengan Tahun Pajak dimaksud telah dibayar"
 			InqStatusError.STATUSERROR = InqError
-			arrTagihan, arrNama = nil, nil
+			arrTagihan, arrNama, arrLunas = nil, nil, nil
 			c.JSON(http.StatusOK, InqStatusError)
 		}
 	default:
@@ -164,7 +183,7 @@ func Inquiry(c *gin.Context) {
 			status.RESPONSECODE = "00"
 			status.ERRORDESC = "Success"
 			resInquiry.STATUS = status
-			arrTagihan, arrNama = nil, nil
+			arrTagihan, arrNama, arrLunas = nil, nil, nil
 			c.JSON(http.StatusOK, resInquiry)
 		}
 	}
@@ -185,4 +204,74 @@ func ambilDenda(jatuhtempo time.Time, pokok uint64) float64 {
 	}
 
 	return denda
+}
+
+func cekLunas(kdKecamatan, kdKelurahan, kdBlok, noUrut, kdJnsOp, tahun string) byte {
+	//var statusLunas byte
+	qry := fmt.Sprintf("select a.STATUS_PEMBAYARAN_SPPT "+
+		"from SPPT a "+
+		"where a.KD_KECAMATAN='%v' "+
+		"and a.KD_KELURAHAN='%v' "+
+		"and a.KD_BLOK='%v' "+
+		"and a.NO_URUT='%v' "+
+		"and a.KD_JNS_OP='%v' "+
+		"and a.THN_PAJAK_SPPT='%v'", kdKecamatan, kdKelurahan, kdBlok, noUrut, kdJnsOp, tahun)
+
+	config.Getenvfile()
+	envUser := os.Getenv("userpbb")
+	envPass := os.Getenv("password")
+	envAddr := os.Getenv("addrpbb")
+	envPort := os.Getenv("portpbb")
+	envSN := os.Getenv("servicenamepbb")
+
+	kon, _ := database.KonekOracle(envUser, envPass, envAddr, envPort, envSN)
+
+	rows, err := kon.Query(qry)
+	if err != nil {
+		logrus.Infof("errornya di baris 99 : %v,", qry)
+	}
+
+	for rows.Next() {
+		if err != rows.Scan(&statusLunas) {
+			logrus.Fatal("error di baris 217 : %v", err.Error())
+		}
+	}
+	defer rows.Close()
+	defer kon.Close()
+	return statusLunas
+}
+
+func cekSPPT(kdKecamatan, kdKelurahan, kdBlok, noUrut, kdJnsOp, tahun string) byte {
+	//var statusDOP byte
+	qry := fmt.Sprintf("select count(a.KD_PROPINSI) "+
+		"from SPPT a "+
+		"where a.KD_KECAMATAN='%v' "+
+		"and a.KD_KELURAHAN='%v' "+
+		"and a.KD_BLOK='%v' "+
+		"and a.NO_URUT='%v' "+
+		"and a.KD_JNS_OP='%v' "+
+		"and a.THN_PAJAK_SPPT='%v' ", kdKecamatan, kdKelurahan, kdBlok, noUrut, kdJnsOp, tahun)
+
+	config.Getenvfile()
+	envUser := os.Getenv("userpbb")
+	envPass := os.Getenv("password")
+	envAddr := os.Getenv("addrpbb")
+	envPort := os.Getenv("portpbb")
+	envSN := os.Getenv("servicenamepbb")
+
+	kon, _ := database.KonekOracle(envUser, envPass, envAddr, envPort, envSN)
+
+	rows, err := kon.Query(qry)
+	if err != nil {
+		logrus.Infof("errornya di baris 257 : %v,", qry)
+	}
+
+	for rows.Next() {
+		if err != rows.Scan(&statusSPPT) {
+			logrus.Fatal("error di baris 262 : %v", err.Error())
+		}
+	}
+	defer rows.Close()
+	defer kon.Close()
+	return statusSPPT
 }
